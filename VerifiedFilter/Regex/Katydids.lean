3f58338c0bf2a9f9093045894b77b32ea732cafa
Walter Schulze <awalterschulze@gmail.com>
2026-06-11 14:55:30 +0100

    fix all sorrys

--- VerifiedFilter/Regex/Katydids.lean
+++ VerifiedFilter/Regex/Katydids.lean
@@ -3,10 +3,10 @@ import VerifiedFilter.Regex.Point
 import VerifiedFilter.Regex.Extracts
 import VerifiedFilter.Regex.Replaces
 
-namespace Regex
-
 def Regex.Point.derives (rs: Vector (Regex (σ × Bool)) l): Vector (Regex σ) l :=
   Vector.map (xs := rs) Regex.Point.derive
+
+namespace Regex
 
 def enters (rs: Vector (Regex σ) l): Vector σ (symcounts rs) :=
   (Regex.extracts rs).2
@@ -15,6 +15,8 @@ def leaves (rs: Vector (Regex σ) l) (bools: Vector Bool (symcounts rs)): (Vecto
   let points: Vector (σ × Bool) (symcounts rs) := Vector.zip (Regex.extracts rs).2 bools
   let replaced: Vector (Regex (σ × Bool)) l := Regex.replaces (Regex.extracts rs).1 points
   Regex.Point.derives replaced
+
+end Regex
 
 def Regex.Katydid.derives (Φ: σ → Bool) (r: Vector (Regex σ) l): Vector (Regex σ) l :=
   enters r |> Vector.map Φ |> leaves r

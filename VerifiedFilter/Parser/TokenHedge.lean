ad1f18f261078d4d680d307dbfd44cf1131fe67a
Walter Schulze <awalterschulze@gmail.com>
2026-06-13 09:11:45 +0100

    add example of how JSON is parsed

--- VerifiedFilter/Parser/TokenHedge.lean
+++ VerifiedFilter/Parser/TokenHedge.lean
@@ -18,3 +18,18 @@ def strnode (s: String) (children: TokenHedge): TokenNode :=
 
 def str (s: String): TokenNode :=
   Hedge.Node.node (Token.string s) []
+
+-- Examples
+
+open Hedge.Node
+open Token
+
+-- The following json: `{"a": ["b", "c"]}`
+-- is parsed as: `{"object": {"a": {"array": {0: "b", 1: "c"}}}}`.
+
+example : Hedge Token :=
+  [node (tag "object") [
+    node (string "a") [
+      node (tag "array") [
+        node (int64 0) [node (string "b") []],
+        node (int64 1) [node (string "c") []]]]]]
